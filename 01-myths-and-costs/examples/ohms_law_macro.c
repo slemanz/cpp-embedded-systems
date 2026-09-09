2d11afb64734b33bc208fd3393c581e24493565d
slemanz <william_sleman@hotmail.com>
2026-09-09 04:15:24 -0300

    myths(feat): add ohms law macro example

---
+++ 01-myths-and-costs/examples/ohms_law_macro.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+#define VOLTAGE 3300
+#define CURRENT 1000
+
+int main(void)
+{
+    const float resistance = VOLTAGE / CURRENT;
+    printf("resistance = %.2f\r\n", resistance);
+
+    return 0;
+}
