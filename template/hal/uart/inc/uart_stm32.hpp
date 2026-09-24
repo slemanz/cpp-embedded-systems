0fe4384660fdf9872e8e95dec709f25caf3c2735
slemanz <william_sleman@hotmail.com>
2026-09-24 04:55:46 -0300

    template(feat): add hal uart_stm32.hpp

---
+++ template/hal/uart/inc/uart_stm32.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstdint>
+
+#include <uart.hpp>
+
+#include <stm32f072xb.h>
+#include <stm32f0xx_hal.h>
+
+namespace hal
+{
+class uart_stm32: public uart
+{
+    public:
+        uart_stm32(USART_TypeDef *inst, std::uint32_t baud = baudrate_default);
+
+        void init() override;
+
+        void putchar(char tx_char) override;
+
+    private:
+        UART_HandleTypeDef huart_;
+        USART_TypeDef *instance_;
+        std::uint32_t baudrate_;
+        static constexpr std::uint32_t baudrate_default = 115200;
+};
+} /* namespace hal*/
