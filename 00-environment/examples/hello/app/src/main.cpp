8a4d7223855f355218cdc9d724ed882bfc31feae
slemanz <william_sleman@hotmail.com>
2026-09-26 11:17:46 -0300

    environment(feat): create hello example

---
+++ 00-environment/examples/hello/app/src/main.cpp
@@ -0,0 +1,27 @@
+#include <cstdint>
+
+#include <hal.hpp>
+#include <stm32f072xb.h>
+#include <uart_stm32.hpp>
+
+int main()
+{
+    hal::init();
+
+    hal::uart_stm32 uart(USART2);
+    uart.init();
+
+    uart.puts("Hello World!\r\n");
+
+    std::uint32_t time_prev = hal::time::get_ms();
+    constexpr std::uint32_t c_1000_ms = 1000;
+
+    while(true)
+    {
+        if(auto time_now = hal::time::get_ms(); time_now - time_prev > c_1000_ms)
+        {
+            uart.puts("While loop 1000 ms ping ...\r\n");
+            time_prev = time_now;
+        }
+    }
+}
